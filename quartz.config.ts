3f23874aacd738dcee13563b582d5c1061c7a556
Gabriel Bakansambu <grielbakansambu@gmail.com>
2026-05-01 22:32:53 +0200

    Quartz sync: May 1, 2026, 10:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "griel-it-garden",
+    baseUrl: "grielbakansambu-glitch.github.io/griel-it-garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
