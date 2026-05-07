3d9596c460fbab2271a9110615cfdd41070d77b3
Gabriel Bakansambu <grielbakansambu@gmail.com>
2026-05-07 22:13:00 +0200

    Quartz sync: May 7, 2026, 10:13 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,17 @@
 import * as Component from "./quartz/components"
-import type { PageLayout } from "./quartz/cfg"
+import type { PageLayout as PageLayout } from "./quartz/cfg"
+
+// Composants globaux
+export const sharedPageComponents = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [],
+  footer: Component.Footer({
+    links: {}
+  }),
+}
+
+// Layout principal (notes)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -21,4 +33,21 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
+}
+
+// Layout listes
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
+
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Explorer(),
+  ],
+
+  right: [],
 }
